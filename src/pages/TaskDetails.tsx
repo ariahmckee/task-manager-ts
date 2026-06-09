f74f0cd2f5e96c33d7ba32ba3d57a8d7d8adf4fd
Ariah <thehittite@gmail.com>
2026-06-09 16:37:49 -0700

    Implement task details page

--- src/pages/TaskDetails.tsx
+++ src/pages/TaskDetails.tsx
@@ -1,7 +1,65 @@
+import { Link, useParams } from "react-router-dom";
+
+import { useTasks } from "../context/TaskContext";
+
 const TaskDetails = () => {
+  const { id } = useParams();
+
+  const { tasks } = useTasks();
+
+  const task = tasks.find(
+    (task) => task.id === id
+  );
+
+  if (!task) {
+    return <h2>Task not found</h2>
+  }
+
   return (
-    <h1>Task Details</h1>
-  )
-}
+    <>
+      <h1>{task.title}</h1>
+
+      <p>
+        <strong>
+          Description:
+        </strong>{" "}
+        {task.description}
+      </p>
+
+      <p>
+        <strong>
+          Status:
+        </strong>{" "}
+        {task.status}
+      </p>
+
+      <p>
+        <strong>
+          Priority:
+        </strong>{" "}
+        {task.priority}
+      </p>
+
+      <p>
+        <strong>
+          Due Date:
+        </strong>{" "}
+        {task.dueDate || "Not set"}
+      </p>
+
+      <Link
+        to={`/edit/${task.id}`}
+      >
+        Edit Task
+      </Link>
+
+      <br />
+
+      <Link to="/">
+        Back to Dashboard
+      </Link>
+    </>
+  );
+};
 
 export default TaskDetails;
