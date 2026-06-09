055bc449e9c9124981608e244f70994d1da01210
Ariah <thehittite@gmail.com>
2026-06-09 11:45:12 -0700

    Build Create Task page

--- src/pages/CreateTask.tsx
+++ src/pages/CreateTask.tsx
@@ -1,7 +1,90 @@
+// CreateTask.tsx
+import { useState } from "react";
+
+import { useNavigate } from "react-router-dom";
+
+import type { TaskPriority, TaskStatus } from "../types/Task";
+
+import { useTasks } from "../context/TaskContext";
+
 const CreateTask = () => {
+  const navigate = useNavigate();
+
+  const { addTask } = useTasks();
+
+  const [title, setTitle] = useState("");
+
+  const [
+    description,
+    setDescription,
+  ] = useState("");
+
+  const [status, setStatus] = useState<TaskStatus>(
+    "To Do"
+  );
+
+  const [
+    priority,
+    setPriority,
+  ] = useState<TaskPriority>(
+    "Medium"
+  );
+
+  const [dueDate, setDueDate] = useState("");
+
+  const handleSubmit = (
+    e: React.FormEvent
+  ) => {
+    e.preventDefault();
+
+    addTask({
+      id: crypto.randomUUID(),
+      title,
+      description,
+      status,
+      description,
+      status,
+      priority,
+      dueDate,
+    });
+
+    navigate("/");
+  };
+
   return (
-    <h1>Create Task</h1>
-  )
-}
+    <form onSubmit={handleSubmit}>
+      <h1>Create Task</h1>
+
+      <input 
+        type="text" 
+        placeholder="Title"
+        value={title}
+        onChange={(e) =>
+          setTitle(
+            e.target.value
+          )
+        }
+      />
+
+      <br />
+
+      <textarea 
+        placeholder="Description"
+        value={description}
+        onChange={(e) =>
+          setDescription(
+            e.target.value
+          )
+        }
+      />
+
+      <br />
+
+      <button type="submit">
+        Save Task
+      </button>
+    </form>    
+  );
+};
 
 export default CreateTask;
